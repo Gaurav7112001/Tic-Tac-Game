20336df0ce6d82fb9ceb9c1d8423d5a99608edcf
Gaurav Kumar Singh <cse.20201004@gmail.com>
2024-05-03 20:05:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let gameover = false;
+
+function makemove(index) {
+  if (!gameover && board[index] === '') {
+    board[index] = currentPlayer;
+    document.getElementById('board').children[index].innerText = currentPlayer;
+    console.log(currentPlayer);
+    checkWinner();
+    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+  }
+}
+
+function checkWinner() {
+  const winningCombination = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+  for (const combo of winningCombination) {
+    const [a, b, c] = combo;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      alert(`${currentPlayer} wins the Game`);
+      gameover = true;
+    }
+  }
+  if (!board.includes('')) {
+    alert('It\'s a tie match');
+    gameover = true;
+  }
+}
+
+function resetGame() {
+  board = ['', '', '', '', '', '', '', '', ''];
+  currentPlayer = 'X';
+  const cells = document.getElementById('board').children;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+  }
+}
